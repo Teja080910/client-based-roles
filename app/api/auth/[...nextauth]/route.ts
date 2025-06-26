47c85f18f1aa40120876e6247dd8426dc9e37368
Teja <tejasimma033@gmail.com>
2025-06-26 10:44:24 +0530

    update: time fromates

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,4 +3,6 @@ import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions)
 
+console.log("date now in auth file", new Date(Date.now()).toLocaleString())
+
 export { handler as GET, handler as POST };
--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,7 @@ import DashboardPage from "./dashboard/page";
 
 export default function Home() {
 
+  console.log("date now in app file", new Date(Date.now()).toLocaleString())
+
   return <DashboardPage />;
 }
