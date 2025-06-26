3ad511f6f83da3a65eeb9bd04ea8fe962c0ff552
Teja <tejasimma033@gmail.com>
2025-06-26 11:03:40 +0530

    update: date method place

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,7 @@
 import { authOptions } from "@/lib/next-auth-options";
 import NextAuth from "next-auth";
 
-const handler = NextAuth(authOptions)
-
 console.log("date now in auth file", new Date(Date.now()).toLocaleString())
 
+const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST };
